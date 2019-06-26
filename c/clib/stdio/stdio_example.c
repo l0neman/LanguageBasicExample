6f6c5e3a4661c73591c594e70be5ac58a173d5a1
WangRuning <wa123fei@gmail.com>
2019-06-26 22:57:59 +0800

    需要改变方法，每次记录目前需要用到的函数，而不是记流水帐。

--- c/clib/stdio/stdio_example.c
+++ c/clib/stdio/stdio_example.c
@@ -35,14 +35,28 @@ int test_tmpfile() {
   return 0;
 }
 
-// fflush
+// fclose - 清空 FILE* 指向的流，并将流中缓冲区写入文件。
+int test_fclose() {
+  FILE *file = fopen("../res/clib_file/stdio/1.txt", "w");
+  if (fclose(file) == EOF) {
+    printf("close error.");
+  }
+
+  return 0;
+}
+
+// fflush - 将流中未写入的数据写入文件。
 int test_fflush() {
+  FILE *file = fopen("../res/clib_file/stdio/1.txt", "w");
+  fwrite(&"1234", 5, 1, file);
+  fflush(file);
+  fclose(file);
   return 0;
 }
 
 // fopen - 打开文件。
 int test_fopen() {
-  FILE *open = fopen("../res/clib_file/stdio/1.txt", "wb");
+  FILE *open = fopen("../res/clib_file/stdio/1.txt", "w");
   if (open != NULL) {
     printf("open file ok.");
   }
@@ -51,13 +65,32 @@ int test_fopen() {
   return 0;
 }
 
+// freopen - 首先尝试关闭和指定流关联的文件，然后将 filename 指定文件
+// 和 FILE* 指向的流关联。
+int test_freopen() {
+  FILE *open = fopen("../res/clib_file/stdio/1.txt", "w");
+  fwrite(&"11", 2, 1, open);
+
+  FILE *f2 = freopen("../res/clib_file/stdio/2.txt", "w", open);
+  if (f2 == NULL) {
+    printf("f2 open error.");
+    return -1;
+  }
+
+  fwrite(&"22", 2, 1, f2);
+  fclose(f2);
+}
 
 int test_stdio_header() {
+  /*
   test_remove();
   test_rename();
   test_tmpfile();
+  test_fclose();
   test_fflush();
   test_fopen();
+  test_freopen();
+  // */
 
   // todo filling test.
   return 0;
